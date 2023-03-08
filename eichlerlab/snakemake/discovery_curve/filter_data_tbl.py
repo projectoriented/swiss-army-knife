2ec8027ceba9b6060184ddaa2a8e531518817d9f
Mei Wu <25568561+projectoriented@users.noreply.github.com>
2023-03-08 10:43:58 -0800

    disc curve script update

--- eichlerlab/snakemake/discovery_curve/filter_data_tbl.py
+++ eichlerlab/snakemake/discovery_curve/filter_data_tbl.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """
 Usage: ./filter_data_tbl.py variants_asd_families_sv_insdel.tsv.gz
 """
@@ -19,5 +18,5 @@ out_fn = insert_str(original_data_tbl, '_filt', insert_pos)
 df = pd.read_csv(original_data_tbl, sep='\t', header=0)
 
 # Write out
-df[(~df['PBSV'].isna()) & (df['VALID'] == 'VALID')].to_csv(out_fn, sep='\t', header=True, index=False)
+df[(~df['PBSV'].isna()) & (df['VAL'] == 'VALID')].to_csv(out_fn, sep='\t', header=True, index=False)
 print(f'file written to: {out_fn}')
