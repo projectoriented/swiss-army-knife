9b851672e93fd00bffbc749532aca4183958706e
Mei Wu <25568561+projectoriented@users.noreply.github.com>
2023-03-10 16:29:56 -0800

    enabling bam fetch

--- eichlerlab/lra/make_ont_fofn.py
+++ eichlerlab/lra/make_ont_fofn.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Usage: ./make_ont_fofn.py --sample 14455_p1 --proj_dir absolute/path/to/clinical --output 14455_p1.fofn --filter_string 'lib=STD;model=sup-prom;bc=guppy;ver=6'
+Usage: ./make_ont_fofn.py --sample 14455_p1 --proj_dir absolute/path/to/clinical --output 14455_p1.fofn --filter_string 'lib=STD;model=sup-prom;bc=guppy;ver=6;ftype=bam'
 This script will get you all the fastqs that belong to guppy ver 6.x.x for STD library
 Slack/email me if you experience any problems or want additional features, wumei@uw.edu
 """
@@ -18,7 +18,8 @@ def make_ont_fofn(sample, fn, prefix, fltr_str):
         'bc': 'guppy',
         'model': 'sup-prom',
         'ver': '6',
-        'lib': 'STD'
+        'lib': 'STD',
+        'ftype': 'fastq'
     }
     if fltr_str:
         alt_fltr_list = fltr_str.split(';')
@@ -30,10 +31,19 @@ def make_ont_fofn(sample, fn, prefix, fltr_str):
     version = fltr_dict['ver']
     library = fltr_dict['lib']
 
-    regex = fr'(.*nanopore)/(?P<lib>{library})/(.*fastq)/(?P<runid>.*)/(?P<bc>{basecaller})/(?P<ver>{version}.*)/(?P<model>{model})/(?P<fn>.*fastq_pass\.fastq\.gz)'
+    file_type = fltr_dict['ftype']
+    if file_type == 'fastq':
+        file_type = 'fastq.gz'
+    elif file_type == 'bam':
+        file_type = 'bam'
+    else:
+        sys.exit(f'Invalid ftype {file_type}. Choose from (fastq, bam)')
+
+    regex = fr'(.*nanopore)/(?P<lib>{library})/(.*fastq)/(?P<runid>.*)/(?P<bc>{basecaller})/(?P<ver>{version}.*)/(?P<model>{model})/(.*fastq_pass)(?P<ftype>.+)'
     search_pattern = re.compile(regex)
     a = []
-    search_path = os.path.join(prefix, sample, 'raw_data/nanopore/*/**/*fastq_pass.fastq.gz')
+
+    search_path = os.path.join(prefix, sample, f'raw_data/nanopore/*/**/*fastq_pass.{file_type}')
 
     file_list = iglob(search_path, recursive=True)
     for f in file_list:
@@ -45,7 +55,7 @@ def make_ont_fofn(sample, fn, prefix, fltr_str):
 
     fofn_df = pd.DataFrame(a)
     if fofn_df.empty:
-        sys.exit(f'No fastq.gz matched the filter- please try again :)')
+        sys.exit(f'No {file_type} matched the filter- please try again :)')
     return fofn_df['fpath'].to_csv(fn, header=False, index=False)
 
 
